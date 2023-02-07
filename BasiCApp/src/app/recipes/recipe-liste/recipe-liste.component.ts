572f3a2e3cf5303bd5dda180eff2f2a06ba4195c
lahcenaitfatoul@gmail.com <lahcenaitfatoul@gmail.com>
2023-02-07 20:46:03 +0100

    struct project

---
+++ BasiCApp/src/app/recipes/recipe-liste/recipe-liste.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-liste',
+  templateUrl: './recipe-liste.component.html',
+  styleUrls: ['./recipe-liste.component.css']
+})
+export class RecipeListeComponent {
+
+}
